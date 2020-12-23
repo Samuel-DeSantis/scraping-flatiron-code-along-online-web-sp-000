a048c4ca35072f6d9fbe54b61fb6f6cf39a870d7
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-12-23 17:38:48 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
+class Course
+  attr_accessor :title, :schedule, :description
 
+  @@all = []
 
+  def initialize
+    save
+  end
+
+  def save
+    self.all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+end
